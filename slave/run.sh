042267412d9303f20880044e740565eebf91909a
kingsman44 <shivan.0972@gmail.com>
2023-11-07 16:12:52 +0530

    Update run.sh

--- slave/run.sh
+++ slave/run.sh
@@ -1,24 +1,25 @@
 chmod +x setup.sh
-chmod +x join.sh
 echo "Started Recieving commands"
 echo "Ctrl + C to Exit"
 while true; do
-	echo "Waiting for command "
-	message="$(nc -l 123)"
-	echo "Command: $message"
-	case "$message" in
-		"setup")
-			echo "Running Setup"
-			./setup.sh
-			;;
-		join*)
-			token=$(echo $message | cut -d' ' -f2-999)
-			echo "join token: $token"
-			"$token"
-			;;
-		"leave")
-			echo "leaving the node"
-			kubeadm reset
-			;;
-	esac
+        echo "Waiting for command "
+        message="$(nc -l 123)"
+        echo "Command: $message"
+        case "$message" in
+                "setup")
+                        echo "Running Setup"
+                        ./setup.sh
+                        ;;
+                join*)
+                        ip=$(echo $message | cut -d' ' -f4)
+                        token=$(echo $message | cut -d' ' -f6)
+                        cert=$(echo $message | cut -d' ' -f8)
+                        echo "join token: $token"
+                        kubeadm join $ip --token $token --discovery-token-ca-cert-hash $cert
+                        ;;
+                "leave")
+                        echo "leaving the node"
+                        kubeadm reset
+                        ;;
+        esac
 done
